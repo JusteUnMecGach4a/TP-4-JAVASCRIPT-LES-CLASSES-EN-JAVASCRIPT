4d3a2916f4a8fe2ef4a2021f22935c22b7ad9ec3
JusteUnMecGach4a <152799916+JusteUnMecGach4a@users.noreply.github.com>
2025-11-12 08:30:09 +0100

    Create formu.js

---
+++ formu.js
@@ -0,0 +1,6 @@
+class NomClasse {
+    constructor(argument1, argument2) {
+        this.attribut1 = argument1; // propriété 1
+        this.attribut2 = argument2; // propriété 2
+        // ... autres propriétés
+    }
