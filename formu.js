2840e8950415e2d2f21f5b66cb4713d56c517ea6
JusteUnMecGach4a <152799916+JusteUnMecGach4a@users.noreply.github.com>
2025-11-12 08:40:32 +0100

    Update formu.js

--- formu.js
+++ formu.js
@@ -1,17 +1,8 @@
 class Voiture {
-    /**
-     * Constructeur pour initialiser la marque et l'année.
-     * @param {string} marque 
-     * @param {number} annee 
-     */
     constructor(marque, annee) {
-        this._marque = marque; // Propriété 1
-        this._annee = annee;   // Propriété 2
+        this._marque = marque; 
+        this._annee = annee;   
     }
-
-    // ===================================
-    // ACCESSEURS (Getters) - Consigne TP E6-TP4
-    // ===================================
 
     getMarque() {
         return this._marque;
@@ -20,10 +11,6 @@ class Voiture {
     getAnnee() {
         return this._annee;
     }
-
-    // ===================================
-    // MUTATEURS (Setters) - Ajoutés pour la logique
-    // ===================================
 
     setMarque(nouvelleMarque) {
         this._marque = nouvelleMarque;
