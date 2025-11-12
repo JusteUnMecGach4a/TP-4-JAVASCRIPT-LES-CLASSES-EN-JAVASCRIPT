92b671aa052821bcb7dd7f80b8c55de0dd86a0ee
JusteUnMecGach4a <152799916+JusteUnMecGach4a@users.noreply.github.com>
2025-11-12 08:44:37 +0100

    Update and rename formu.js to fichierClasseJavascript.js

--- fichierClasseJavascript.js
+++ fichierClasseJavascript.js
@@ -21,6 +21,51 @@ class Voiture {
     }
 }
 
+
+function creationObjetsClasse() {
+    const maRenault = new Voiture("Renault", 2023);
+    const maPeugeot = new Voiture("Peugeot", 2020);
+
+    const conteneur = document.getElementById('resultats-voitures');
+    conteneur.innerHTML = ' '; 
+
+    console.log("--- Instanciation et Affichage Console ---");
+    
+    console.log(`Objet 1 : ${maRenault.getMarque()} (${maRenault.getAnnee()})`);
+    
+    console.log(`Objet 2 : ${maPeugeot.getMarque()} (${maPeugeot.getAnnee()})`);
+    console.log("----------------------------------------");
+    
+    const pRenault = document.createElement('p');
+    pRenault.textContent = `Voiture 1 : Marque = ${maRenault.getMarque()}, Année = ${maRenault.getAnnee()}.`;
+    conteneur.appendChild(pRenault);
+    
+    const pPeugeot = document.createElement('p');
+    pPeugeot.textContent = `Voiture 2 : Marque = ${maPeugeot.getMarque()}, Année = ${maPeugeot.getAnnee()}.`;
+    conteneur.appendChild(pPeugeot);
+}class Voiture {
+    constructor(marque, annee) {
+        this._marque = marque; 
+        this._annee = annee;   
+    }
+
+    getMarque() {
+        return this._marque;
+    }
+
+    getAnnee() {
+        return this._annee;
+    }
+
+    setMarque(nouvelleMarque) {
+        this._marque = nouvelleMarque;
+    }
+
+    setAnnee(nouvelleAnnee) {
+        this._annee = nouvelleAnnee;
+    }
+}
+
 // =====================================
 // INSTANCIATION (Consigne)
 // =====================================
