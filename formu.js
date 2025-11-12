16cbd5eb48e737d31882070e2681f50bc1b5e9c9
JusteUnMecGach4a <152799916+JusteUnMecGach4a@users.noreply.github.com>
2025-11-12 08:33:26 +0100

    Update formu.js

--- formu.js
+++ formu.js
@@ -1,6 +1,28 @@
 class Voiture {
     constructor(marque, annee) {
-        this.marque = marque;
-        this.annee = annee;
+        // Les propriétés sont souvent préfixées d'un underscore (_)
+        // lorsqu'elles sont gérées par des Setters/Getters.
+        this._marque = marque; 
+        this._annee = annee;
+    }
+
+    // 1. GETTER (Accesseur) pour la marque
+    get marque() {
+        return this._marque;
+    }
+
+    // 2. SETTER (Mutateur) pour la marque
+    set marque(nouvelleMarque) {
+        this._marque = nouvelleMarque;
+    }
+
+    // 3. GETTER (Accesseur) pour l'année
+    get annee() {
+        return this._annee;
+    }
+
+    // 4. SETTER (Mutateur) pour l'année
+    set annee(nouvelleAnnee) {
+        this._annee = nouvelleAnnee;
     }
 }
